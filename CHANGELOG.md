234ae2eadb18f7db52d6bc213c8c6646c20ebea2
Hannes Braun <hannesbraun@mail.de>
2021-04-29 22:08:58 +0200

    Add support for more or less than two channels

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.1.1
+* Support for reading wav files with more or less than two channels
+* Improved error handling
+# 0.1.0
+*Initial version*
--- wav2csv.go
+++ wav2csv.go
@@ -7,20 +7,29 @@ import (
 	"github.com/youpy/go-wav"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
+	// Command line flags
 	inPath := flag.String("in", "", "wav file to read")
 	outPath := flag.String("out", "out.csv", "csv output file")
 	flag.Parse()
 
+	// Validate input path
+	if len(strings.TrimSpace(*inPath)) == 0 {
+		panic("Please specify an input file")
+	}
+
+	// Wav file reader
 	in, err := os.Open(*inPath)
 	if err != nil {
 		panic(err)
 	}
-	reader := wav.NewReader(in)
 	defer in.Close()
+	reader := wav.NewReader(in)
 
+	// Buffered csv output
 	out, err := os.Create(*outPath)
 	if err != nil {
 		panic(err)
@@ -28,16 +37,35 @@ func main() {
 	defer out.Close()
 	bufout := bufio.NewWriter(out)
 
+	format, err := reader.Format()
+	if err != nil {
+		panic(err)
+	}
+	channels := format.NumChannels
+
 	for {
+		// Reading samples
 		samples, err := reader.ReadSamples()
 		if err == io.EOF {
 			break
 		}
 
+		// Iterate over given samples
 		for _, sample := range samples {
-			ch0 := fmt.Sprintf("%f", reader.FloatValue(sample, 0))
-			ch1 := fmt.Sprintf("%f", reader.FloatValue(sample, 1))
-			_, err := bufout.WriteString(ch0 + "," + ch1 + "\n")
+			row := ""
+
+			// Iterate over channels
+			for i := uint16(0); i < channels; i++ {
+				if i == 0 {
+					row += fmt.Sprintf("%f", reader.FloatValue(sample, uint(i)))
+				} else {
+					row += fmt.Sprintf(",%f", reader.FloatValue(sample, uint(i)))
+				}
+			}
+			row += "\n"
+
+			// Write row to file
+			_, err := bufout.WriteString(row)
 			if err != nil {
 				panic(err)
 			}
