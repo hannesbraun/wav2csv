129fcdddb8bd9707fbc60184c234df8ff4ba076f
Hannes Braun <hannesbraun@mail.de>
2021-05-03 17:14:11 +0200

    Add LICENSE

--- wav2csv.go
+++ wav2csv.go
@@ -10,7 +10,13 @@ import (
 	"strings"
 )
 
+const VERSION = "0.1.2"
+
 func main() {
+	fmt.Println("wav2csv", VERSION)
+	fmt.Println("Copyright (c) 2021, Hannes Braun")
+	fmt.Println()
+
 	// Command line flags
 	inPath := flag.String("in", "", "wav file to read")
 	outPath := flag.String("out", "out.csv", "csv output file")
